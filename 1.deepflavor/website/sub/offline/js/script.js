be64a6f4cb12635731e0deb03befbb06a766718c
jiuuni-oss <aveda1026@gmail.com>
2026-04-26 12:59:12 +0900

    Add files via upload

---
+++ 1.deepflavor/website/sub/offline/js/script.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: "A64F30.swiper-button-prev",
+    },
+});
+
+
+
